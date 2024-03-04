18a2bc18d534e9e0178e9b88246f282c20ccc0a5
kotarella1110 <kotarella1110@gmail.com>
2024-03-04 17:56:15 +0900

    chore: add readme

---
+++ scripts/copy-readme-to-packages.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+import glob from "fast-glob";
+
+(async () => {
+  const packages = await glob("packages/*", {
+    onlyDirectories: true,
+    absolute: true,
+  });
+
+  for (const packageDir of packages) {
+    await fs.promises.copyFile(
+      path.join(__dirname, "../README.md"),
+      path.join(packageDir, "README.md"),
+    );
+  }
+})();
