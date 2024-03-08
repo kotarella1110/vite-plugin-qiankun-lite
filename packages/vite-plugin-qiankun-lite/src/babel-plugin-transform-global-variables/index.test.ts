9bcb505b8083879b6e7544ba0f1a1b37c92a5233
Kotaro Sugawara <kotarella1110@gmail.com>
2024-03-08 21:11:39 +0900

    feat: enhance sandbox mode (#7)
    * feat: enhance sandbox mode
    
    * Create friendly-feet-smell.md
    
    * fix: fix isReplaceableIdentifiers condition
    
    * test: update babel test
    
    * refactor: refactor babel plugin
    
    * refactor: refactor babel plugin

--- packages/vite-plugin-qiankun-lite/src/babel-plugin-transform-global-variables/index.test.ts
+++ packages/vite-plugin-qiankun-lite/src/babel-plugin-transform-global-variables/index.test.ts
@@ -8,6 +8,7 @@ pluginTester({
       document: '__TEST__["test"].document',
       window: "__TEST__.window",
       self: "__TEST__.self",
+      "process.env.TEST": "__TEST__.process.env.TEST",
     },
   } as Exclude<Parameters<typeof plugin>[1], null>,
   tests: {
@@ -16,29 +17,50 @@ pluginTester({
         console.log("window", window);
         console.log("document", document);
         console.log("self", self);
+        console.log("process.env.TEST", process.env.TEST);
+      `,
+      snapshot: true,
+    },
+    "variable snapshot": {
+      code: `
+        const window = "test";
+        const document = "test";
+        const self = "test";
+        const process = {
+          env: {
+            TEST: "test",
+          },
+        }
+        window;
+        document;
+        self;
+        process.env.TEST
       `,
       snapshot: true,
     },
     "IIFE snapshot": {
       code: `
-        (function(window, document, self) {
+        (function(window, document, self, process) {
           window;
           document;
           self;
+          process.env.TEST;
         })(window, document, self);
       `,
       snapshot: true,
     },
     "function snapshot": {
       code: `
-        function test(window, document, self) {
+        function test(window, document, self, process) {
           window;
           document;
           self;
+          process.env.TEST;
         }
         window;
         document;
         self;
+        process.env.TEST;
       `,
       snapshot: true,
     },
@@ -76,14 +98,63 @@ pluginTester({
       code: `
         const test1 = {
           window: window,
+          get window() {
+            return window;
+          },
+          set window(window) {},
           document: document,
+          get document() {
+            return document;
+          },
+          set document(document) {},
           self: self,
+          get self() {
+            return self;
+          },
+          set self(self) {},
+          "process.env.TEST": process.env.TEST,
         };
         const test2 = {
           window,
           document,
           self,
         };
+      `,
+      snapshot: true,
+    },
+    "class snapshot": {
+      code: `
+        class Test {
+          #window
+          self
+          constructor(window, self, process) {
+            this.window = window;
+            this.self = self;
+            this.test = process.env.TEST;
+          }
+          document() {
+            return document;
+          }
+          self = () => {
+            return this.self;
+          }
+          get window() {
+            return this.#window;
+          }
+          set window(window) {
+            this.#window = window;
+          }
+        }
+      `,
+      snapshot: true,
+    },
+    "import statement snapshot": {
+      code: `
+        import window, { document, self, process } from "test";
+        window;
+        document;
+        self;
+        process.env.TEST
       `,
       snapshot: true,
     },
--- packages/vite-plugin-qiankun-lite/src/babel-plugin-transform-global-variables/index.ts
+++ packages/vite-plugin-qiankun-lite/src/babel-plugin-transform-global-variables/index.ts
@@ -1,4 +1,5 @@
 import { type NodePath, types as t } from "@babel/core";
+import generator from "@babel/generator";
 import { declare } from "@babel/helper-plugin-utils";
 import { parse } from "@babel/parser";
 
@@ -11,48 +12,128 @@ export default declare<Options>((api, options) => {
   return {
     visitor: {
       Identifier(path) {
-        if (isReplaceableExpressions(path, replacementExpressions)) {
-          const name = path.node.name;
-          const replacementIdentifier = replacementExpressions[name];
+        const replaceableIdentifiers = Object.values(replacementExpressions)
+          .map(({ from }) => from)
+          .filter((from): from is t.Identifier => t.isIdentifier(from));
+        if (isReplaceableIdentifier(path, replaceableIdentifiers)) {
+          const replacementIdentifier =
+            replacementExpressions[path.node.name].to;
           path.replaceWith(replacementIdentifier);
+        }
+      },
+      MemberExpression(path) {
+        const replaceableMemberExpressions = Object.values(
+          replacementExpressions,
+        )
+          .map(({ from }) => from)
+          .filter((from): from is t.MemberExpression =>
+            t.isMemberExpression(from),
+          );
+        if (isReplaceableMemberExpression(path, replaceableMemberExpressions)) {
+          const replacementMemberExpression =
+            replacementExpressions[generator(path.node).code].to;
+          path.replaceWith(replacementMemberExpression);
         }
       },
     },
   };
 });
 
-function isReplaceableExpressions(
+function isReplaceableIdentifier(
   path: NodePath<t.Identifier>,
-  identifiers: Record<string, t.Expression>,
+  replaceableIdentifiers: t.Identifier[],
 ) {
   return (
-    // biome-ignore lint/suspicious/noPrototypeBuiltins: <explanation>
-    identifiers.hasOwnProperty(path.node.name) &&
+    replaceableIdentifiers.some(
+      (replaceableIdentifier) => replaceableIdentifier.name === path.node.name,
+    ) &&
     !path.scope.hasBinding(path.node.name) &&
     !path.parentPath.isMemberExpression({ property: path.node }) &&
-    !path.parentPath.isObjectProperty({ key: path.node })
+    !path.parentPath.isObjectProperty({ key: path.node }) &&
+    !path.parentPath.isObjectMethod({ key: path.node }) &&
+    !path.parentPath.isClassProperty({ key: path.node }) &&
+    !path.parentPath.isClassMethod({ key: path.node }) &&
+    !path.parentPath.isPrivateName({ id: path.node })
   );
+}
+
+function isReplaceableMemberExpression(
+  path: NodePath<t.MemberExpression>,
+  replaceableMemberExpressions: t.MemberExpression[],
+) {
+  const deepestNodePath = getDeepestNodePath(path);
+  return (
+    replaceableMemberExpressions.some((replaceableMemberExpression) =>
+      isMatchingMemberExpression(path.node, replaceableMemberExpression),
+    ) &&
+    t.isIdentifier(deepestNodePath.node) &&
+    !deepestNodePath.scope.hasBinding(deepestNodePath.node.name) &&
+    !path.parentPath.isMemberExpression({ object: path.node })
+  );
+}
+
+function isMatchingMemberExpression(
+  pathNode: t.PrivateName | t.Expression,
+  targetPathNode: t.PrivateName | t.Expression,
+): boolean {
+  if (t.isMemberExpression(pathNode) && t.isMemberExpression(targetPathNode)) {
+    return (
+      isMatchingMemberExpression(pathNode.object, targetPathNode.object) &&
+      isMatchingMemberExpression(pathNode.property, targetPathNode.property)
+    );
+  }
+  if (t.isIdentifier(pathNode) && t.isIdentifier(targetPathNode)) {
+    return pathNode.name === targetPathNode.name;
+  }
+  if (
+    (t.isStringLiteral(pathNode) && t.isStringLiteral(targetPathNode)) ||
+    (t.isNumericLiteral(pathNode) && t.isNumericLiteral(targetPathNode))
+  ) {
+    return pathNode.value === targetPathNode.value;
+  }
+  return false;
+}
+
+function getDeepestNodePath(path: NodePath<t.MemberExpression>) {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  let deepestNodePath: any = path;
+  while (deepestNodePath.node.object) {
+    deepestNodePath = deepestNodePath.get("object");
+  }
+  return deepestNodePath as NodePath<t.Node>;
 }
 
 function getReplacementExpressions(replace: Record<string, string>) {
-  return Object.keys(replace).reduce(
-    (object, name) =>
-      Object.assign(object, {
-        [name]: parseToExpression(replace[name]),
-      }),
-    {} as Record<string, t.Expression>,
-  );
+  type ReplacementExpressions = Record<
+    string,
+    {
+      from: t.MemberExpression | t.Identifier;
+      to: t.MemberExpression | t.Identifier;
+    }
+  >;
+  return Object.keys(replace).reduce((acc, from) => {
+    const fromMemberExpressionOrIdentifier =
+      parseToMemberExpressionOrIdentifier(from);
+    return Object.assign(acc, {
+      [from]: {
+        from: fromMemberExpressionOrIdentifier,
+        to: parseToMemberExpressionOrIdentifier(replace[from]),
+      },
+    });
+  }, {} as ReplacementExpressions);
 }
 
-function parseToExpression(code: string) {
+function parseToMemberExpressionOrIdentifier(
+  code: string,
+): t.MemberExpression | t.Identifier {
   const ast = parse(code);
   const statement = ast.program.body[0];
   if (!t.isExpressionStatement(statement)) {
     throw new Error("Expected ExpressionStatement");
   }
   const expression = statement.expression;
-  if (t.isMemberExpression(expression) && t.isIdentifier(expression)) {
+  if (!t.isMemberExpression(expression) && !t.isIdentifier(expression)) {
     throw new Error("Expected MemberExpression or Identifier");
   }
-  return expression as t.Expression;
+  return expression;
 }
