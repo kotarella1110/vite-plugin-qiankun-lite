6c4e50628fe40cfa4b7dc46c7c2f2cbccc1c51ab
kotarella1110 <kotarella1110@gmail.com>
2024-03-13 15:59:03 +0900

    test: add sandbox e2e test

--- examples/main/src/main.tsx
+++ examples/main/src/main.tsx
@@ -76,7 +76,7 @@ registerMicroApps(
 
 setDefaultMountApp("/react");
 
-start({ sandbox: true });
+start({ sandbox: import.meta.env.SANDBOX });
 
 runAfterFirstMounted(() => {
   console.log(`[${name}] first app mounted`);
